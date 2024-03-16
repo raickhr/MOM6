ce922fc02980bb4a2ae47050268a6fcd2de6b5a1
Shikhar Rai <ckhr.rai@gmail.com>
2024-03-16 14:53:45 -0400

    fixed a typo

--- config_src/drivers/solo_driver/user_surface_forcing.F90
+++ config_src/drivers/solo_driver/user_surface_forcing.F90
@@ -145,7 +145,7 @@ subroutine USER_wind_forcing(sfc_state, forces, day, G, US, CS)
   !  is always positive.
   if (associated(forces%ustar)) then ; do j=js,je ; do i=is,ie
     !  This expression can be changed if desired, but need not be.
-    forces%ta
+
     u_mag(i,j) = G%mask2dT(i,j) * (CS%gust_const + &
             sqrt(0.5*(forces%taux(I-1,j)**2 + forces%taux(I,j)**2) + &
                  0.5*(forces%tauy(i,J-1)**2 + forces%tauy(i,J)**2)))
